484d627c73ed4770e2305124c487582fe3de4d77
ernestognw <ernestognw@gmail.com>
2019-05-04 22:47:44 -0500

    Se añadio funcionalidad a la grafica

--- src/App.js
+++ src/App.js
@@ -1,61 +1,63 @@
-import React, { Component } from 'react';
-import { Line } from 'react-chartjs-2';
-import logo from './logo.svg';
-import './App.css';
-
-class Button extends Component{
-  render(){
-    return(
-      <button>{this.props.text}</button>
-    )
-  }
-}
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/button";
 
 class App extends Component {
-  handleTaskChange = (event) =>
-  this.setState({ newTask: event.target.value })
+  handleTaskChange = event => this.setState({ newTask: event.target.value });
 
   constructor(props) {
-      super(props)
+    super(props);
 
-      this.state = {
-         newTask: "sdfasdf",
-         myData: {
-           labels: ["1","2","3","4", "5","6"],
-           datasets: [
-
-
-             {
-               label: "Atlas",
-               data: [
-                 1, 3, 22, 9, 12, 11
-               ]
-             },
-             {
-               label: "Atlas 2",
-               backgroundColor: "#34323",
-               data: [
-                 12, 31, 1, 29, 12, 13
-               ]
-             }
-           ]
-
+    this.state = {
+      newTask: "sdfasdf",
+      myData: {
+        labels: ["1", "2", "3", "4", "5", "6"],
+        datasets: [
+          {
+            label: "Atlas",
+            data: [1, 3, 22, 9, 12, 11]
+          }
+        ]
       }
-}
-   }
+    };
+  }
 
-  render(){
-    return(
+  addData = () => {
+    this.setState({
+      myData: {
+        labels: [...this.state.myData.labels, Date.now()],
+        datasets: [
+          {
+            ...this.state.myData.datasets[0],
+            data: [...this.state.myData.datasets[0].data, Math.random() * 30]
+          }
+        ]
+      }
+    });
+  };
+
+  render() {
+    return (
       <div className="container">
-     <h1 className="title">
-        Hello world <span aria-label="emoji" role="img">🔥</span>
-     </h1>
-     <Button text="Este es mi primer boton" />
-     <input value={this.state.newTask} onChange={this.handleTaskChange} type="text" className="new-task"/>
-    <h2 className="test-label">{ this.state.newTask}</h2>
-    <Line data={this.state.myData} />
-  </div>
-);
+        <h1 className="title">
+          Hello world{" "}
+          <span aria-label="emoji" role="img">
+            🔥
+          </span>
+        </h1>
+        <input
+          value={this.state.newTask}
+          onChange={this.handleTaskChange}
+          type="text"
+          className="new-task"
+        />
+        <h2 className="test-label">{this.state.newTask}</h2>
+        <Line data={this.state.myData} />
+        <Button addData={this.addData} text="Este es mi primer boton" />
+      </div>
+    );
   }
 }
 
